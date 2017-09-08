093c9e79799a74c5f8423415e34dda2f5592b520
Petter Källström <petter.kallstrom@enfogroup.com>
2017-09-08 14:59:49 +0200

    chore: initial commit

---
+++ HoloYenga/Assets/Cube.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class Cube : MonoBehaviour {
+
+    private Color[] colors = new Color[8];
+    private Color currentColor;
+
+
+    // Use this for initialization
+    void Start () {
+
+        colors[0] = Color.cyan;
+        colors[1] = Color.red;
+        colors[2] = Color.green;
+        colors[3] = Color.black;
+        colors[4] = Color.yellow;
+        colors[5] = Color.magenta;
+        colors[6] = Color.blue;
+        colors[7] = Color.gray;
+
+        currentColor = colors[0];
+
+        GenerateCubes(0.3f, true);
+
+        GenerateCubes(0.6f, false);
+
+        GenerateCubes(0.9f, true);
+
+        GenerateCubes(1.2f, false);
+
+        GenerateCubes(1.5f, true);
+
+        GenerateCubes(1.8f, false);
+
+        GenerateCubes(2.1f, true);
+
+        GenerateCubes(2.4f, false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void GenerateCubes(float spawnHeightY, bool xAxis)
+    {
+        // 
+        float j = 0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            j = j + 0.35f;
+            CreateCube(j, spawnHeightY, xAxis);
+        }
+        
+    }
+
+    // space to set cubes side by side, spawnYspace for spawn height, xAxis to set the "zigzag" pattern
+    void CreateCube(float space, float spawnYspace, bool xAxis)
+    {
+
+        // Create object position and size
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+       
+        if (xAxis)
+        {
+            cube.transform.position = new Vector3(0, spawnYspace, space);
+            cube.transform.localScale = new Vector3(1f, 0.25f, 0.33f);
+        }
+        else
+        {
+            cube.transform.position = new Vector3(space - 0.66f, spawnYspace, 0.66f);
+            cube.transform.localScale = new Vector3(0.33f, 0.25f, 1f);
+        }
+           
+        // Get new color for the cube, avoid last used color
+        Color color = ColorRandomizer(currentColor);
+        cube.GetComponent<Renderer>().material.color = color;
+        currentColor = color;
+
+        // Give cube physics
+        Rigidbody gameObjectsRigidBody = cube.AddComponent<Rigidbody>();
+
+        // Attach HandDraggable script to cube
+        cube.AddComponent<HandDraggable>();
+    }
+
+    // Return a random color as long as it's not the last one used.
+    Color ColorRandomizer(Color lastColor)
+    {
+        Color nextColor = lastColor;
+
+        do
+        {
+            nextColor = colors[Random.Range(0, colors.Length)];
+
+        } while (nextColor == lastColor);
+
+        return nextColor;
+    }
+}
---
+++ HoloYenga/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,45 @@
+﻿using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    static int sphereCount;
+    int sphereNumber;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<HandDraggable>().StartedDragging += OnStartedDragging;
+        this.gameObject.GetComponent<HandDraggable>().StoppedDragging += OnStoppedDragging;
+
+      
+    }
+    void OnStartedDragging()
+    {
+        // When we start dragging we clear out any existing world anchor for this
+        // Xmas present.
+        WorldAnchorManager.Instance.RemoveAnchor(this.gameObject);
+
+        // We also add the RigidBody component so that (e.g.) you can't push
+        // the present through the floor.
+        var rigidBody = this.gameObject.AddComponent<Rigidbody>();
+
+        // But we don't want it to fall on the floor on its own.
+        rigidBody.useGravity = false;
+    }
+    void OnStoppedDragging()
+    {
+        // We take away the RigidBody because it doesn't play well with
+        // Rigidbody.
+        Destroy(this.gameObject.GetComponent<Rigidbody>());
+
+        // Now remember where the present was.
+        WorldAnchorManager.Instance.AttachAnchor(this.gameObject, this.sphereNumber.ToString());
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        // If we do get a collision then we (a bit rudley) just stop the
+        // drag operation.
+        this.gameObject.GetComponent<HandDraggable>().StopDragging();
+    }
+}
